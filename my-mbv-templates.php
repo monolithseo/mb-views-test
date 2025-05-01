fa478b6c914128d174f3c1a8a05c3609fcb4b8f6
monolithseo <oliver@monolithseo.com>
2025-05-01 09:56:11 +0700

    feat: add section namespace

--- my-mbv-templates.php
+++ my-mbv-templates.php
@@ -11,3 +11,8 @@ add_filter('mbv_fs_paths', function($paths) {
     $paths[] = plugin_dir_path(__FILE__) . 'templates';
     return $paths;
 });
+
+// Register the section namespace
+add_action('mbv_fs_loader_init', function($fs_loader) {
+    $fs_loader->addPath(plugin_dir_path(__FILE__) . 'templates/section', 'section');
+});
